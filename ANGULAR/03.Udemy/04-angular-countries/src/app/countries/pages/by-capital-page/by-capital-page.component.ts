b3c783134db800a54f473d7dc2e8d94cc6aab4df
Milton Vasquez <milton.vasquez@kineticadvantage.com>
2024-05-07 14:44:29 -0500

    all

---
+++ ANGULAR/03.Udemy/04-angular-countries/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'app-by-capital-page',
+  templateUrl: './by-capital-page.component.html',
+  styles: [
+  ]
+})
+export class ByCapitalPageComponent {
+
+  public countries: Country[] = [];
+  isloading: boolean = false;
+
+  constructor( private countriesService: CountriesService ) {}
+
+  searchByCapital( term: string ):void  {
+
+    this.isloading = true;
+    this.countriesService.searchCapital( term )
+      .subscribe( countries => {
+        this.countries = countries;
+        this.isloading = false;
+      });
+
+  }
+
+}
---
+++ ANGULAR/03.Udemy/04-angular-countries/src/app/shared/components/search-box/search-box.component.ts
@@ -0,0 +1,48 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
+
+@Component({
+  selector: 'shared-search-box',
+  templateUrl: './search-box.component.html',
+  styles: [],
+})
+export class SearchBoxComponent implements OnInit, OnDestroy {
+  private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscription?: Subscription;
+
+  @Input()
+  public placeholder: string = '';
+
+  @Output()
+  public onValue = new EventEmitter<string>();
+
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  emitValue(value: string): void {
+    this.onValue.emit(value);
+  }
+
+  onKeypress(searchTerm: string): void {
+    this.debouncer.next(searchTerm);
+  }
+
+  ngOnInit(): void {
+    this.debouncerSubscription = this.debouncer
+      .pipe(debounceTime(1000))
+      .subscribe((value) => {
+        this.onDebounce.emit(value);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe();
+  }
+}
